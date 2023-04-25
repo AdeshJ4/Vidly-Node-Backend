eb6479ec2a3e43e696ad18c750fd714d283b2124
AdeshJ4 <adejad1234@gmail.com>
2023-04-25 14:56:03 +0530

    Added prod.js route

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const dotenv = require("dotenv").config();
 const debug = require("debug")("app:startup");
 const app = express();
 
-require("./startup/dbConnection")(); // connectDB();
+require("./startup/dbConnection")();
 require('./startup/config')();
+require('./startup/prod')(app);
 require('./startup/routes')(app);
 
 const port = process.env.PORT || 3001;
--- startup/routes.js
+++ startup/routes.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const helmet = require("helmet");
 const morgan = require("morgan");
 const genres = require("../routes/genres");
 const customers = require("../routes/customers");
@@ -7,14 +6,10 @@ const movies = require("../routes/movies");
 const rentals = require("../routes/rentals");
 const users = require("../routes/users");
 const errorHandler = require("../middlewares/errorHandler");
-const compression = require("compression");
 const config = require("config");
-
 
 module.exports = function (app) {
   app.use(express.json());
-  app.use(helmet());
-  app.use(compression());
   if (config.get("NODE_ENV") === "development") app.use(morgan("tiny"));
   app.use("/api/customers", customers);
   app.use("/api/movies", movies);
